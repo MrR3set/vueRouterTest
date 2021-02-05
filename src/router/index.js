b98c627a89ce6be29e4c478c65e1bb6f2de121b0
MrR3set <bairon.pazrobles@gmail.com>
2021-02-04 21:31:22 -0800

    Set up routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HelloWorld from '../components/HelloWorld'
+import Page1 from '../components/Page1'
+import Page2 from '../components/Page2'
 
 Vue.use(VueRouter)
 
@@ -9,6 +11,16 @@ const routes = [
     path: '/',
     name: 'Home',
     component: HelloWorld
+  },
+  {
+    path: '/page1',
+    name: 'Page1',
+    component: Page1
+  },
+  {
+    path: '/page2',
+    name: 'Page2',
+    component: Page2
   },
 ]
 
