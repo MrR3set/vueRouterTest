4bd29529de6e737b0e98bd88e5118b7a08b39b29
MrR3set <bairon.pazrobles@gmail.com>
2021-02-04 21:30:59 -0800

    Created dummy pages

---
+++ src/components/Page1.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="page1">
+    <h1>Page 1</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Page1'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ src/components/Page2.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="page2">
+    <h1>Page 2</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Page2'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
